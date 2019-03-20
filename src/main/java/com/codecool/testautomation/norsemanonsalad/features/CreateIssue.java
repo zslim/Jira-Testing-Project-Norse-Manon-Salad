40d4cdda3936160d288430c7c21206d8d3df1441
bencebachraty <bakybence@gmail.com>
2019-03-20 14:52:18 +0100

    added validate method for successful issue creation and refactored dropdown selection

--- src/main/java/com/codecool/testautomation/norsemanonsalad/features/CreateIssue.java
+++ src/main/java/com/codecool/testautomation/norsemanonsalad/features/CreateIssue.java
@@ -1,9 +1,13 @@
 package com.codecool.testautomation.norsemanonsalad.features;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CreateIssue extends Feature {
 
@@ -30,23 +34,23 @@ public class CreateIssue extends Feature {
         PageFactory.initElements(driver, this);
     }
 
-    void selectProject(String projectName) {
-        projectField.sendKeys(projectName);
+    void selectFromDropdown(WebElement element, String select) {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
 
-        if (projectField.getAttribute("aria-activedescendant").equals("null")) {
-            throw new IllegalArgumentException();
-        }
-    }
+        element.sendKeys(select);
 
-    void selectIssueType(String issueName) {
-        issueTypeField.sendKeys(issueName);
-
-        if (issueTypeField.getAttribute("aria-activedescendant").equals("null")) {
+        if (!element.getAttribute("aria-activedescendant").equals("null")) {
+            element.sendKeys(Keys.ENTER);
+        } else {
             throw new IllegalArgumentException();
         }
     }
 
     void fillInSummaryField(String summaryMessage) {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(summaryField));
+
         summaryField.sendKeys(summaryMessage);
     }
 
@@ -55,9 +59,22 @@ public class CreateIssue extends Feature {
     }
 
     void createNewIssue(String projectName, String issueName, String summary) {
-        selectProject(projectName);
-        selectIssueType(issueName);
+        selectFromDropdown(projectField, projectName);
+        selectFromDropdown(issueTypeField, issueName);
         fillInSummaryField(summary);
         submitNewIssue();
+    }
+
+    boolean validateSuccessfulIssueCreation(String issueName) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("aui-flag-container")));
+
+        WebElement conformation = driver.findElement(By.xpath("//a[@class='issue-created-key issue-link']"));
+
+        if (conformation.getText().contains(issueName)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
