cea5be6a946d35b12acf88fd5cf5e7e1b7e4f552
Zsofia Limbek <zsofia.limbek@gmail.com>
2019-03-21 20:39:39 +0100

    added version deleting method

--- src/main/java/com/codecool/testautomation/norsemanonsalad/features/ProjectVersions.java
+++ src/main/java/com/codecool/testautomation/norsemanonsalad/features/ProjectVersions.java
@@ -1,7 +1,6 @@
 package com.codecool.testautomation.norsemanonsalad.features;
 
 import com.codecool.testautomation.norsemanonsalad.testutils.Utils;
-import jdk.jshell.execution.Util;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -24,8 +23,20 @@ public class ProjectVersions extends Feature {
     @FindBy(name = "description")
     WebElement descriptionField;
 
-    @FindBy(xpath = "//table[@id='versions-table']/tbody[2]/tr[1]/td[@class='versions-table__name']//a")
+    @FindBy(xpath = "//table[@id='versions-table']//td[@class='versions-table__name'][1]//a")
     WebElement nameOfVersionOnTop;
+
+    @FindBy(css = "span.aui-iconfont-more")
+    WebElement actionButtonOfSingleVersion;
+
+    @FindBy(css = "a.project-config-operations-delete")
+    WebElement deleteOption;
+
+    @FindBy(id = "submit")
+    WebElement confirmDeleteButton;
+
+    @FindBy(css = "span.aui-iconfont-approve")
+    WebElement approveSign;
 
     private static final String RELEASE_PAGE_URL = "https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page";
 
@@ -47,7 +58,20 @@ public class ProjectVersions extends Feature {
         nameField.submit();
     }
 
+    public void deleteVersion(String versionName) {
+        String versionFilterUrl = "https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.atlassian.jira.jira-projects-plugin:release-page&status=unreleased&contains=" + versionName;
+        driver.get(versionFilterUrl);
+        waitUntilElementLoaded(newReleaseForm);
+
+        actionButtonOfSingleVersion.click();
+        waitUntilElementLoaded(deleteOption);
+        deleteOption.click();
+        waitUntilElementLoaded(confirmDeleteButton);
+        confirmDeleteButton.click();
+    }
+
     public String validateNewVersion() {
+        waitUntilElementLoaded(approveSign);
         return nameOfVersionOnTop.getText();
     }
 
