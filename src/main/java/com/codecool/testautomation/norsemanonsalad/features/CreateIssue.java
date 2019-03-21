188462bd992930bb3586a4d8e2518efff2bda48f
bencebachraty <bakybence@gmail.com>
2019-03-21 10:31:25 +0100

    added method to delete created issues

--- src/main/java/com/codecool/testautomation/norsemanonsalad/features/CreateIssue.java
+++ src/main/java/com/codecool/testautomation/norsemanonsalad/features/CreateIssue.java
@@ -1,8 +1,10 @@
 package com.codecool.testautomation.norsemanonsalad.features;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -94,6 +96,43 @@ public class CreateIssue extends Feature {
         selectFromDropdown(issueTypeField, issueName);
         fillInSummaryField(summary);
         clickCancelButton();
+    }
+
+    //Delete issue after test
+    void deleteIssue(String issueName) {
+        driver.navigate().to("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+
+        driver.findElement(By.id("find_link")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("find_link-content")));
+
+        driver.findElement(By.id("issues_new_search_link_lnk")).click();
+
+        WebElement searchField = driver.findElement(By.id("searcher-query"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(searchField));
+
+        searchField.sendKeys(issueName);
+        searchField.sendKeys(Keys.ENTER);
+
+        wait.until(ExpectedConditions.textToBe(By.id("project-name-val"), "Private Project 3"));
+
+        driver.findElement(By.id("opsbar-operations_more")).click();
+
+        WebElement delete = driver.findElement(By.xpath("//*[@id='delete-issue']/a"));
+        Actions action = new Actions(driver);
+
+        action.moveToElement(delete);
+        action.perform();
+
+        wait.until(ExpectedConditions.visibilityOf(delete));
+
+        delete.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("delete-issue-submit")));
+
+        driver.findElement(By.id("delete-issue-submit")).click();
     }
 
     boolean validateSuccessfulIssueCreation(String issueName) {
