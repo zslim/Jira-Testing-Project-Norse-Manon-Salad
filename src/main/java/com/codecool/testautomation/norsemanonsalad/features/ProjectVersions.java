75ac829c048ee9195e27d6a888bf048c8bc91afd
Zsofia Limbek <zsofia.limbek@gmail.com>
2019-03-21 19:57:07 +0100

    fixed locators & field filling

--- src/main/java/com/codecool/testautomation/norsemanonsalad/features/ProjectVersions.java
+++ src/main/java/com/codecool/testautomation/norsemanonsalad/features/ProjectVersions.java
@@ -1,8 +1,11 @@
 package com.codecool.testautomation.norsemanonsalad.features;
 
+import com.codecool.testautomation.norsemanonsalad.testutils.Utils;
+import jdk.jshell.execution.Util;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ProjectVersions extends Feature {
 
@@ -18,16 +21,17 @@ public class ProjectVersions extends Feature {
     @FindBy(name = "releaseDate")
     WebElement releaseDateField;
 
-    @FindBy(name = "dascription")
+    @FindBy(name = "description")
     WebElement descriptionField;
 
-    @FindBy(xpath = "//table[@id='versions-table']/tbody[contains(concat=(' ', @class, ' '), ' items ')]/tr[1]/td[@class='versions-table__name']/a")
+    @FindBy(xpath = "//table[@id='versions-table']/tbody[2]/tr[1]/td[@class='versions-table__name']//a")
     WebElement nameOfVersionOnTop;
 
     private static final String RELEASE_PAGE_URL = "https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page";
 
     public ProjectVersions(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public void navigateToReleases() {
@@ -36,10 +40,10 @@ public class ProjectVersions extends Feature {
     }
 
     public void addNewVersion(String versionName, String startDate, String releaseDate, String description) {
-        nameField.sendKeys(versionName);
-        startDateField.sendKeys(startDate);
-        releaseDateField.sendKeys(releaseDate);
-        descriptionField.sendKeys(description);
+        nameField.sendKeys(Utils.nullToEmptyString(versionName));
+        startDateField.sendKeys(Utils.nullToEmptyString(startDate));
+        releaseDateField.sendKeys(Utils.nullToEmptyString(releaseDate));
+        descriptionField.sendKeys(Utils.nullToEmptyString(description));
         nameField.submit();
     }
 
