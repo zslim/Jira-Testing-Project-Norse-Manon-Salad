127f50ebd8f936ca93768601ab4ca3df0f7fdc09
bencebachraty <bakybence@gmail.com>
2019-03-21 15:36:00 +0100

    created CreateIssueTest class with test cases

---
+++ src/test/java/com/codecool/testautomation/norsemanonsalad/features/CreateIssueTest.java
@@ -0,0 +1,74 @@
+package com.codecool.testautomation.norsemanonsalad.features;
+
+import com.codecool.testautomation.norsemanonsalad.testutils.Utils;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateIssueTest {
+
+    private Login login;
+    private CreateIssue createIssue;
+    private WebDriver driver;
+    private static final String FAIL_TEST_DATA = "/create_issue_fail.csv";
+    private static final String PASS_TEST_DATA = "/create_issue_pass.csv";
+    private static final String PROJECT_TYPES_TEST_DATA = "/create_issue_project_with_types.csv";
+
+    @BeforeAll
+    static void init() {
+        Utils.setDriverPath();
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = Utils.createDriver();
+        login = new Login(driver);
+        createIssue = new CreateIssue(driver);
+        login.loginSuccessful();
+    }
+
+    @AfterEach
+    void tearDown() {
+        createIssue.closeDriver();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = PASS_TEST_DATA, numLinesToSkip = 1)
+    void testSuccessfulIssueCreation(String projectName, String issueType, String summary) {
+        createIssue.clickCreateButton();
+
+        createIssue.createNewIssue(projectName, issueType, summary);
+
+        assertTrue(createIssue.validateSuccessfulIssueCreation());
+
+        createIssue.deleteIssue(summary);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = FAIL_TEST_DATA, numLinesToSkip = 1)
+    void testUnsuccessfulIssueCreationWithEmptySummary(String projectName, String issueType, String summary) {
+        createIssue.clickCreateButton();
+
+        createIssue.createNewIssue(projectName, issueType, summary);
+
+        assertTrue(createIssue.validateEmptySummaryError());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = PROJECT_TYPES_TEST_DATA, numLinesToSkip = 1)
+    void testProjectHasAllRequiredTypes(String projectName) {
+        createIssue.clickCreateButton();
+
+        List<String> expectedTypes = createIssue.getRequiredTypes();
+
+        String actualResult = createIssue.getProjectTypes(projectName, expectedTypes);
+
+        assertEquals(expectedTypes.toString(), actualResult);
+    }
+
+}
