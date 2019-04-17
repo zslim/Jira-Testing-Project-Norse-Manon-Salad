5ee96d49847c35f3b8cfc551f7594f4985f0fe81
Zsofia Limbek <zsofia.limbek@gmail.com>
2019-04-17 10:32:23 +0200

    changed Firefox driver to remote driver

--- src/main/java/com/codecool/testautomation/norsemanonsalad/testutils/Utils.java
+++ src/main/java/com/codecool/testautomation/norsemanonsalad/testutils/Utils.java
@@ -3,13 +3,17 @@ package com.codecool.testautomation.norsemanonsalad.testutils;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Objects;
 
 public class Utils {
 
     public static final String BASE_URL = "https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa";
+    public static final String NODE_URL = "http://selenium:CCPass123@seleniumhub.codecool.codecanvas.hu:4444/wd/hub";
 
     public static void setDriverPath() {
         String driverProperty = getEnvironmentVar("DRIVER_PROPERTY");
@@ -17,13 +21,20 @@ public class Utils {
         System.setProperty(driverProperty, driverPath);
     }
 
-    public static WebDriver createDriver(){
+    public static WebDriver createDriver() {
         String preferredBrowser = getEnvironmentVar("BROWSER");
         WebDriver driver;
         if (preferredBrowser.equals("Chrome")) {
             driver = new ChromeDriver();
         } else if (preferredBrowser.equals("Firefox")) {
-            driver = new FirefoxDriver();
+            FirefoxOptions options = new FirefoxOptions();
+            URL nodeUrl = null;
+            try {
+                nodeUrl = new URL(NODE_URL);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            driver = new RemoteWebDriver(nodeUrl, options);
         } else {
             throw new NotSupportedBrowserException("Browser specified in env var not supported, please choose Chrome or Firefox");
         }
